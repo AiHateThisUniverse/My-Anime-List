4490a3c0a8551e1f46eebad07fb088ca63846adf
IraHumanitas <irahumanitas@gmail.com>
2024-07-17 07:49:55 +0700

    Last OH

---
+++ admin/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$alamatserver   = "localhost";
+$pengguna       = "root";
+$katakunci      = "";
+$namadatabase   = "anime222";
+
+$varkoneksi = mysqli_connect($alamatserver, $pengguna, $katakunci, $namadatabase); 
+
+?>
